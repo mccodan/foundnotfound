23f3e188b9bb3770b9c094e3be5794fb20f2fd7a
Cole McCain <mccainc@vmware.com>
2024-06-12 14:23:38 -0600

    Initial crude href report

--- foundnotfound.py
+++ foundnotfound.py
@@ -1,62 +1,34 @@
-import re, csv
-from datetime import datetime
+import re
 from urllib.request import urlopen
+import requests
 
 '''Open Broadcom support knowledge article.'''
 url = input("What knowledge article are you needing to check?")
-page = urlopen(url)
-html = page.read().decode("utf-8")
+# Present yourself to support.broadcom.com as a Firefox iPad
+HEADERS = {'User-Agent': 'Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148'}
 
-# #csv file creation
-# filename = (str(datetime.now())[:+19]).replace(":", "").replace("-", "").replace(" ", "_") + '.csv'
-# header = ['StoryType', 'StoryID', 'StoryKHeadline']
-# data = []
-# #StoryType can be RS or OS, related stories or other stories
+r = requests.get(url, headers=HEADERS)
 
-# Get all url's within a "wolken-content-container" div
-body = "wolken-content-container.*?>.*Was this article helpful?"
-rs_results = re.findall(body, html, re.IGNORECASE)
-print(rs_results)
-# y = 0
 
-# #cleanup output and print
-# print("Related Stories:" + "\n")
-# if (len(rs_results) ==8):
-#     rs_results = rs_results[4:8]
-# for x in rs_results:
-#     title = re.sub("<.*?>", "", x)
-#     title = title.removeprefix("BigLittle-title\">")
-#     y = y+1
-#     data.append([ "RS", y, title])
-#     print(title)
 
-# #Get the other stories
-# pattern = "ListItem-title.*?>.*</h3>"
-# os_results = re.findall(pattern, html, re.IGNORECASE)
-# y = 0
+hrefs = re.findall('<a href=.*?</a>', r.text)
+#<a href="https://kb.vmware.com/s/article/1002282" target="_blank">KB 1002282</a>
 
-# #print("\n\nOther Stories:" + "\n")
-# for x in os_results:
-#     title = re.sub("<.*?>", "", x)
-#     title = title.removeprefix("ListItem-title\">")
-#     y = y + 1
-#     data.append([ "OS", y, title])
-#     print(title)
+hrefList = ""
 
-# #write the data to a csv
-# with open(filename, 'w') as file:
-#     for header in header:
-#         file.write(str(header)+', ')
-#     file.write('\n')
-#     for row in data:
-#         for x in row:
-#             file.write(str(x)+', ')
-#         file.write('\n')
+# need to make a list of 2 property objects: url and <a> text
+for h in hrefs:
+    # if h contains "https://" add everything between "" to list                         <---- add the link
+    if '://' in h:
+        hrefList += h + "\n"
+    # if line contains </a>, get everything between first "> and </a> to get the text    <---- add the text
+print("\nLinks found: \n" + hrefList) 
 
-# time = datetime.now()
-# local_now = time.astimezone()  #local_now is the local time with the UTC offset, signifying timezone
-# local_tz = local_now.tzinfo
-# time = str(time)[:-7]
-# #print(time, local_tz)
+# linkList = "Links: "
+# for h in hrefList:
+#     if '"' in h:
+#         linkList += re.search(".*?", h)
 
-# #print(filename)
+# print(linkList)
+# linkList = ""
+# build the 2 property list
